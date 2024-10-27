84a43735f5f102c22c19cb837ebd0608cc7c7957
alfredjmgdev <alfredjmgdev@gmail.com>
2024-10-27 15:13:04 -0400

    fix: adding populate

--- content-creator-app-back/src/repositories/theme.repository.ts
+++ content-creator-app-back/src/repositories/theme.repository.ts
@@ -6,11 +6,13 @@ export class ThemeRepository {
   }
 
   async findById(id: string): Promise<ITheme | null> {
-    return await Theme.findById(id);
+    return await Theme.findById(id)
+      .populate('categoriesIds', 'type label _id');
   }
 
   async findAll(): Promise<ITheme[]> {
-    return await Theme.find({ deletedAt: null });
+    return await Theme.find({ deletedAt: null })
+      .populate('categoriesIds', 'type label _id');;
   }
 
   async update(id: string, themeData: Partial<ITheme>): Promise<ITheme | null> {
